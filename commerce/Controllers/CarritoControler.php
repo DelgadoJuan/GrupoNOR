57286007b8ed171989b205ac393f078ab00b97ee
nicocerezo09 <nico.cerezo@hotmail.com>
2024-06-05 11:26:54 -0300

    revirtiendo commit

--- commerce/Controllers/CarritoControler.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../Models/Detalle_pedido.php';
-include_once '../Util/Config/config.php';
-
-$detalle_pedido = new Detalle_pedido();
-
-session_start();
-
--- commerce/Controllers/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-//controlador para cerrar sesion
-session_start();
-session_destroy();
-header('Location: ../Views/index.php');
-
